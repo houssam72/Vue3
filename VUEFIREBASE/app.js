10b922b4c8ca51e4d142a4b6749fc0afb43830f4
houssam72 <brothersmarket72@gmail.com>
2023-05-23 18:29:48 +0200

    Add Introduction and Basic1

---
+++ VUEFIREBASE/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+  // data,functions
+  //   template: "<h2>I am the template </h2>",
+
+  data() {
+    return {
+      showBooks: true,
+      title: "The final Empire",
+      author: "Brandon Sanderson",
+      age: 45,
+    };
+  },
+  methods: {
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
+    },
+  },
+});
+
+app.mount("#app");
