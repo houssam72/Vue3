cc973c7318c5c78370cc093759c699688f1e28f3
houssam72 <brothersmarket72@gmail.com>
2023-05-24 10:30:58 +0200

    Add Leeson 2

--- VUEFIREBASE/app.js
+++ VUEFIREBASE/app.js
@@ -4,15 +4,40 @@ const app = Vue.createApp({
 
   data() {
     return {
+      url: "http://www.thenetninja.co.uk",
       showBooks: true,
-      title: "The final Empire",
-      author: "Brandon Sanderson",
-      age: 45,
+      books: [
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "the way of king",
+          author: "brandon sanderson",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "The final empire",
+          author: "patrick rothfuss",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
+      ],
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+  },
+  computed: {
+    fileteredBooks() {
+      return this.books.filter((book) => {
+      return  book.isFav;
+      });
     },
   },
 });
