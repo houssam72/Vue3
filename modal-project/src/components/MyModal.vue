d34f6f2aeb5ca913d20220eb40420d47aa9afc94
houssam72 <brothersmarket72@gmail.com>
2023-05-26 01:46:16 +0200

    Add lesson 4

--- modal-project/src/components/MyModal.vue
+++ modal-project/src/components/MyModal.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="backdrop">
-    <div class="modal">
-      <p>Modal content</p>
+  <div class="backdrop" @click.self="toogleModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot>Default content c'est afficher quand y'as de Slot</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["theme", "toogleModal"],
+};
+</script>
 
 <style>
 .modal {
@@ -20,5 +29,38 @@
   background: rgba(0, 0, 0, 0.5);
   width: 100%;
   height: 100%;
+}
+.modal h1 {
+  color: #03cfb4;
+  border: none;
+  padding: 0;
+}
+.modal p {
+  font-style: normal;
+}
+
+.modal .actions{
+  text-align: center;
+  margin: 30px 0 10px 0;
+  color: #333;
+}
+
+.modal .actions a{
+  color: #333;
+  padding: 8px;
+  border:1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+
+}
+
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+
+.modal.sale h1 {
+  color: white;
 }
 </style>
