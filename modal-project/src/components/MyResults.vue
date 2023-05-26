40f8bc1988e203a37220b0695ff1c15014b974d6
houssam72 <brothersmarket72@gmail.com>
2023-05-26 17:12:06 +0200

    Add lesson5 And Reaction TimeGame

---
+++ modal-project/src/components/MyResults.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>Reaction time : {{ score }}</p>
+    <p>{{ rank }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Ninja Fingers";
+    } else if (this.score < 400) {
+      this.rank = "Rapid Reflex";
+    } else {
+      this.rank = "Snail pace .......";
+    }
+  },
+};
+</script>
+
+<style></style>
